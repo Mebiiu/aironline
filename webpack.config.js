e01199958db25c8bc4b48ba7d652ac6534df2f14
Mebiiu <497377347@qq.com>
2016-07-11 10:30:24 +0800

    ls

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+
+const webpack = require('webpack');
+
+module.exports = {
+  entry:[
+  'webpack-dev-server/client?http://localhost:3000',
+  'webpack/hot/only-dev-server',
+  './app/main.js'],
+  output:{
+    path:__dirname+'/build/',
+    publicPath: 'http://127.0.0.1:3000/build/',
+    filename:'bundle.js',
+  },
+ module: {
+  loaders: [
+    { test: /\.css$/, loader: 'style!css' },
+    { test: /\.jsx?$/, loader: 'jsx?harmony'},
+   
+    {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'react-hot!jsx-loader?harmony'
+  }
+  ]
+},
+  resolve: {
+  extensions: ['', '.js', '.jsx']
+	},
+	plugins: [
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': '"development"'
+}),
+new webpack.HotModuleReplacementPlugin()
+  ]
+   
+}
