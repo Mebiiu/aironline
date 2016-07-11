b4221c0fb5c2a22114f683498bc6f84225eabb43
Mebiiu <497377347@qq.com>
2016-07-11 13:46:58 +0800

    first

--- app/main.js
+++ app/main.js
@@ -1,4 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require("./components/app");
-ReactDOM.render(<App name="Nate" />, document.getElementById("app"));
+var TagList = [<a href="#">Facebook</a>,
+  <a href="#">Google</a>];
+ReactDOM.render(<App list={TagList} />, document.getElementById("app"));
+
+	
